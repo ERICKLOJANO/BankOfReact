1e793650447dfa4d3c3586111a7bb6374ec11e60
Ariel Benzur <aab171@outlook.com>
2021-01-13 22:56:47 -0500

    Commit 2: App Pages made

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+          <center>
+            <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank"/>
+            <h1>Bank of React</h1>
+            <h2><AccountBalance accountBalance={this.props.accountBalance}/></h2>
+            <Link to="/userProfile">User Profile </Link>
+            <Link to="/logIn"> Login</Link>
+          </center>
+        </div>
+    );
+  }
+}
+
+export default Home;
